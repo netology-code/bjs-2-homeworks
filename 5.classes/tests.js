1e8fed657349ea58f62c5a56b8756a0415f4b25e
copperfox <copperfox@mail.ru>
2021-05-21 09:40:14 +0300

    task 5

---
+++ 5.classes/tests.js
@@ -0,0 +1,135 @@
+describe('Домашнее задание к занятию 2.3 «ООП в JS (ES6)»', () => {
+
+  describe('Задача №1', () => {
+    let printItem;
+
+    beforeEach(function(){
+      printItem = new PrintEditionItem('Типовой школьный журнал', 2019, 102);
+    });
+
+    it('создание печатного издания', () => {
+      expect(printItem).toBeDefined();
+      expect(printItem.name).toEqual('Типовой школьный журнал');
+      expect(printItem.releaseDate).toEqual(2019);
+      expect(printItem.pagesCount).toEqual(102);
+      expect(printItem.state).toEqual(100);
+      expect(printItem.type).toEqual(null);
+    });
+  
+    it('починка печатного издания', () => {
+      printItem.state = 90;
+      printItem.fix();
+      expect(printItem.state).toEqual(100);
+    });  
+    
+    it('геттер для свойства state', () => {
+      printItem.state = 10;
+      const spy = spyOnProperty(printItem, 'state', 'get').and.returnValue(10);
+      expect(printItem.state).toBe(10);
+      expect(spy).toHaveBeenCalled();
+    });  
+    
+    it('сеттер для свойства state', () => {
+      const spy = spyOnProperty(printItem, 'state', 'set');
+      printItem.state = 10;
+      expect(spy).toHaveBeenCalled();
+    });    
+    
+    it('создание объекта Magazine', () => {
+      printItem = new Magazine('Forbes', 2020, 180);
+      expect(printItem.type).toEqual("magazine");
+    });   
+    
+    it('создание объекта Book', () => {
+      printItem = new Book('А. Сапковский', 'Меч Предназначения', 1992, 384);
+      expect(printItem.author).toEqual('А. Сапковский');
+      expect(printItem.type).toEqual('book');
+    });  
+    
+    it('создание объекта NovelBook', () => {
+      printItem = new NovelBook('А. Сапковский', 'Меч Предназначения', 1992, 384);
+      expect(printItem.author).toEqual('А. Сапковский');
+      expect(printItem.type).toEqual('novel');
+    });
+    
+    it('создание объекта FantasticBook', () => {
+      printItem = new FantasticBook('Джон Толкин', 'Властелин колец', 1954, 2093);
+      expect(printItem.author).toEqual('Джон Толкин');
+      expect(printItem.type).toEqual('fantastic');
+    });
+    
+    it('создание объекта DetectiveBook', () => {
+      printItem = new DetectiveBook('Агата Кристи', 'Десять негритят', 2019, 256);
+      expect(printItem.author).toEqual('Агата Кристи');
+      expect(printItem.type).toEqual('detective');
+    }); 
+  });
+
+  describe('Задача №2', () => {
+    let library, printItem;
+  
+    beforeEach(function(){
+      library = new Library('Библиотека имени Ленина');
+      printItem = new PrintEditionItem('Типовой школьный журнал', 2019, 102);
+    });
+
+    it('создание библиотеки', () => {
+      expect(library).toBeDefined();
+      expect(library.name).toEqual('Библиотека имени Ленина');
+      expect(library.books).toEqual(jasmine.any(Array));
+    });   
+    
+    it('добавление книги', () => {
+      library.addBook(printItem);
+      expect(library.books[0].name).toEqual('Типовой школьный журнал');
+      expect(library.books.length).toEqual(1);
+    });   
+    
+    it('поиск книги', () => {
+      library.addBook(printItem);
+      const firstBook = library.findBookBy("releaseDate", 2019);
+      expect(firstBook.name).toEqual('Типовой школьный журнал');
+      const secondBook = library.findBookBy("releaseDate", 2154);
+      expect(secondBook).toEqual(null);
+    });   
+    
+    it('выдача книги', () => {
+      library.addBook(printItem);
+      const firstBook = library.giveBookByName('Типовой школьный журнал');
+      expect(firstBook.name).toEqual('Типовой школьный журнал');
+      expect(library.books.length).toEqual(0);
+      const secondBook = library.giveBookByName('Судовой журнал');
+      expect(secondBook).toEqual(null);
+    });
+  })
+
+  describe('Задача №3', () => {
+    let studentLog;
+  
+    beforeEach(function(){
+      studentLog = new StudentLog("Иван Петров");
+    });
+
+    it('создание объекта StudentLog', () => {
+      expect(studentLog).toBeDefined();
+    });
+  
+    it('получение имени студента', () => {
+      expect(studentLog.getName()).toEqual("Иван Петров");
+    });
+  
+    it('подсчёт средней оценки по предмету', () => {
+      studentLog.addGrade(3, "algebra");
+      studentLog.addGrade(5, "algebra");
+      expect(studentLog.getAverageBySubject("algebra")).toEqual(4);
+    });
+  
+    it('подсчёт общей средней оценки', () => {
+      studentLog.addGrade(3, "algebra");
+      studentLog.addGrade(5, "algebra");
+      studentLog.addGrade(5, "history");
+      studentLog.addGrade(5, "history");
+      expect(studentLog.getTotalAverage()).toEqual(4.5);
+    });
+  });
+});
