1552e711059968ca99de4002706fbcca864dd72f
copperfox <copperfox@mail.ru>
2021-05-30 23:14:14 +0300

    fix task 3 arrays

--- 3.arrays/tests.js
+++ 3.arrays/tests.js
@@ -4,7 +4,7 @@ describe("Домашнее задание к занятию 3 «Массивы»
       expect(compareArrays([1, 2, 3], [1, 2, 3])).toEqual(true)
     })
 
-    it("[1,2,3] === [1,2,3] false", () => {
+    it("[1, 2], [1, 2, 3] false", () => {
       expect(compareArrays([1, 2], [1, 2, 3])).toEqual(false)
     })
 
@@ -12,10 +12,10 @@ describe("Домашнее задание к занятию 3 «Массивы»
       expect(compareArrays([1, 2, 3], [3, 2, 1])).toEqual(false)
     })
 
-    it("[0,1,2] === [0, 1 ] false", () => {
-      expect(compareArrays([1, 2, 3], [3, 2, 1])).toEqual(false)
+    it("[0,1,2] === [0, 1] false", () => {
+      expect(compareArrays([0,1,2] === [0, 1])).toEqual(false)
     })
-    it("[1, 2 ] === [1,2,3] false", () => {
+    it("[8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5] false", () => {
       expect(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])).toEqual(false)
     })
   })
@@ -28,6 +28,9 @@ describe("Домашнее задание к занятию 3 «Массивы»
     })
     it("[-1,-2] => []  true", () => {
       expect(advancedFilter([-1, -2])).toEqual([])
+    })
+    it("[4,3,5] => [30]  true", () => {
+      expect(advancedFilter([4, 3, 5])).toEqual([30])
     })
   })
 })
