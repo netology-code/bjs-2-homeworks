8152a0ed224733a2d9a0ad53275dca68a2c89d6e
Kasum5 <70338595+Kasum5@users.noreply.github.com>
2021-05-31 17:05:03 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,16 @@
 function solveEquation(a, b, c) {
-  let arr;
+  "use strict"
   // код для задачи №1 писать здесь
+  let arr = [];
+  let D = b**2 - 4*a*c; // формула дискримента
+
+ if (D === 0) {
+     arr.push((-b + Math.sqrt(D)) / (2 *a));
+ } else if (D > 0) {
+     arr.push((-b + Math.sqrt(D)) / (2 *a));
+     arr.push((-b - Math.sqrt(D)) / (2 *a));
+ };
+  
   return arr; // array
 }
 
