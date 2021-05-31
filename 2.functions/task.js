e134b2bbf79dcad565d72911f0e57d9191246210
Siun  Maksim <myrdym@rambler.ru>
2021-05-31 15:27:59 +0300

    Tasks Completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,40 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] <  min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+    avg = sum/arr.length;
+  }
+  
 
-  123
-
-  return { min:min, max:max, avg:avg };
+  return { min:min, max:max, avg:avg.toFixed(2) };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  123
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  123
-  // for ...
-  
-  return max
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++)  {
+    let sum = func(arrOfArr[i]);
+    if (max < sum)
+      max = sum;
+    }
+  return max;
 }
+
