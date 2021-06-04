8e8af19d8e1136be9e12e64f43dabd6433f878a5
theart84 <artshadow74@gmail.com>
2021-06-04 13:34:24 +0500

    done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age= age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks = [...this.marks, ...marks];
+  } else {
+    this.marks = [...this.marks, ...marks];
+  }
+}
+
+Student.prototype.getAverage = function () {
+  if (this.marks === undefined) {
+    return;
+  }
+  return this.marks.reduce((acc, value) => acc + value, 0) / this.marks.length;
+}
+
+Student.prototype.excludeStudent = function(reason) {
+  if (this.subject === undefined) {
+    return 'Студент не участвует в учебном процессе!'
+  }
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
