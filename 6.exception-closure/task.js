03f31629ec1b261d1d3ed5310dddddb084063fa9
theart84 <artshadow74@gmail.com>
2021-06-11 22:05:51 +0500

    done

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const parseCount = (value) => {
+  const result = Number.parseInt(value);
+  if (result) {
+    return result;
+  } else {
+    throw new Error("Невалидное значение");
+  }
+};
+
+const validateCount = (value) => {
+  try {
+    return parseCount(value);
+  } catch (e) {
+    return e;
+  }
+};
+
+class Triangle {
+  constructor(sideA, sideB, sideC) {
+    if (
+      !(sideB + sideC > sideA && sideA + sideC > sideB && sideA + sideB > sideC)
+    ) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
+  }
+
+  getPerimeter() {
+    return this.sideA + this.sideB + this.sideC;
+  }
+  getArea() {
+    const halfPerimeter = this.getPerimeter() / 2;
+    return Number(
+      Math.sqrt(
+        halfPerimeter *
+          (halfPerimeter - this.sideA) *
+          (halfPerimeter - this.sideB) *
+          (halfPerimeter - this.sideC)
+      ).toFixed(3)
+    );
+  }
+}
+
+const getTriangle = (sideA, sideB, sideC) => {
+  try {
+    return new Triangle(sideA, sideB, sideC);
+  } catch (e) {
+    return {
+      getArea() {
+        return "Ошибка! Треугольник не существует";
+      },
+      getPerimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+};
