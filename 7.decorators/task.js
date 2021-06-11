ec788c42d330c7996b399246fa003dc947344e0a
copperfox <copperfox@mail.ru>
2021-06-12 02:14:34 +0300

    fixes + task 7 tests

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,7 +1,28 @@
 function cachingDecoratorNew(func) {
+  let cache = {};
+
+  function wrapper(...args) {
+      const hash = args.join(',');
+
+      if (hash in cache) {
+          console.log("Из кеша: " + cache[hash]);
+          return "Из кеша: " + cache[hash];
+      } else {
+          let result = func(...args);
+          cache[hash] = result;
+          console.log("Вычисляем: " + result);
+          return "Вычисляем: " + result;
+      }
+  }
+
+  return wrapper;
+}
+
+
+function debounceDecoratorNew(func) {
   // Ваш код
 }
 
-function debounceDecoratorNew(func) {
+function debounceDecorator2(func) {
   // Ваш код
 }
