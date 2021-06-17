0f1cad64b69d697832a4517c7b69adc0c398eef8
copperfox <copperfox@mail.ru>
2021-06-17 12:46:56 +0300

    fixes

--- 4.objects/tests.js
+++ 4.objects/tests.js
@@ -3,22 +3,23 @@ describe("Домашнее задание к занятию 4 «Объекты»
     let student1 = new Student("Tony", "male", 37);
     student1.setSubject("Algebra");
     student1.addMark(5);
-    student1.addMark(4);
+    student1.addMark(4); 
     student1.addMark(5);
     
     let student2 = new Student("Buzz", "female", 35);
     student2.setSubject("Geometry");
-    student2.addMark(2);
-    student2.addMark(3);
-    student2.addMark(2);
+    student2.addMarks(2,3,2);
 
     it("Установка предмета работает корректно", () => {
       expect(student1.subject).toEqual("Algebra");
       expect(student2.subject).toEqual("Geometry");
     });
 
-    it("Добавление оценок работает корректно", () => {
+    it("Добавление оценок addMark работает корректно", () => {
       expect(student1.marks).toEqual([5, 4, 5]);
+    });
+
+    it("Добавление группы оценок addMarks работает корректно", () => {
       expect(student2.marks).toEqual([2, 3, 2]);
     });
 
