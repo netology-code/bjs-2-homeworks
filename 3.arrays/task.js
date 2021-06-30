6c05b62f0bb37d21422063fc3be3ae6985cca8bd
Aleksandr Shatskikh <work@errand.ru>
2021-06-30 22:07:10 +0300

    prod: sync

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -36,3 +36,40 @@ function advancedFilter(arr) {
 
   return resultArr; // array
 }
+
+
+/*
+*
+* попробуйте такую функцию написать: принимает массив чисел и значение суммы,
+* а возвращает значение количества чисел, которое необходимо суммировать до получения переданной суммы.
+* Если набранной суммы не хватает, то возвращает общее количество элементов.
+*
+//возвращает 4, т.к. 1+2+3=6 (3 числа), а 1+2+3+5=11 (4 числа)
+*
+getValuestCountToSumValues([1,2,3,5,2,7,3,5,2], 10);
+*
+//возвращает 6, т.к. 1+2+3+5+2=13 (5 чисел), а 1+2+3+5+2+7=20 (6 чисел)
+*
+getValuestCountToSumValues([1,2,3,5,2,7,3,5,2], 20);
+*
+Функция должна сразу возвращать результат. Не должно быть никаких циклов или дополнительных переменных.
+*
+* */
+
+function getValuesCountToSumValues(arr, sum) {
+
+  if(Array.isArray(arr) && Number.isInteger(sum)) {
+
+    arr.reduce((acc, el) => {
+      acc++;
+      console.log('el ' + el);
+      console.log('acc ' + acc);
+    }, 0);
+
+  } else {
+    return;
+  }
+}
+
+console.log('Mine');
+console.log(getValuesCountToSumValues([1,2,3,5,2,7,3,5,2], 10));
