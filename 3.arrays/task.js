e9921a29c85a9fdf01bbafe2c494026f5034f43b
Aleksandr Shatskikh <work@errand.ru>
2021-06-30 16:42:24 +0300

    feat: #3 task is ready to go

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if(Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length) {
+
+    arr1.every((el, i) => {
+      result = el === arr2[i];
+    });
+
+  } else {
+    result = false;
+  }
+
 
   return result; // boolean
 }
@@ -9,7 +20,19 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  if(Array.isArray(arr)) {
+    resultArr = arr.filter(
+        num => num > 0
+    ).filter(
+        num => num % 3 === 0
+    ).map(
+        num => num * 10
+    );
+
+  } else {
+    console.log(`arr is not array`);
+    return;
+  }
 
   return resultArr; // array
 }
