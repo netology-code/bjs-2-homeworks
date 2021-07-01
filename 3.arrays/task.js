f1cab0f483f34ba2c08b3880c5b46cfe7c6c4fc1
shamagir <shamagir@gmail.com>
2021-07-01 20:12:23 +0300

    fix: refactoring code

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,14 +3,13 @@ function compareArrays(arr1, arr2) {
   return (
     arr1.length === arr2.length &&
     arr1.every((item, idx) => item === arr2[idx])
-  )
+  );
 }
 
 function advancedFilter(arr) {
   // Ваш код
-  return (arr
+  return arr
     .filter((item) => item > 0 && item % 3 === 0)
-    .map((item) => item * 10)
-  );
+    .map((item) => item * 10);
 }
 
