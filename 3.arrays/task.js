4d94c1066bf636e089e4599dd587d2ba1f0580ab
shamagir <shamagir@gmail.com>
2021-07-01 12:38:33 +0300

    fix: format code

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,7 +2,12 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
-  if(arr1.length === arr2.length && arr1.every(function (item, idx) {return item === arr2[idx];})) {
+  if (
+    arr1.length === arr2.length &&
+    arr1.every(function (item, idx) {
+      return item === arr2[idx];
+    })
+  ) {
     result = true;
   } else {
     result = false;
@@ -15,13 +20,17 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-  resultArr = arr.filter((item) => {
-    if(item > 0) return item; 
-  }).filter((item) => {
-    return item % 3 === 0;
-  }).map((item) => {
-    return item * 10;
-  });
+  resultArr = arr
+    .filter((item) => {
+      if (item > 0) return item;
+    })
+    .filter((item) => {
+      return item % 3 === 0;
+    })
+    .map((item) => {
+      return item * 10;
+    });
 
   return resultArr; // array
 }
+
