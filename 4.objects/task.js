a15ed4cea66c87b6b5a29ffb8170d483730734b6
shamagir <shamagir@gmail.com>
2021-07-05 16:22:09 +0300

    feat: bjs-2-homeworks 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
     // Ваш код
-
+    this.name   = name;
+    this.gender = gender;
+    this.age    = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  //ваш код
+  if(this.marks === undefined){ 
+    this.marks = [mark]; 
+    } else {
+      // добавить вторую и последующие оценки в массив
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  console.log(marks);
+  marks.forEach(el => this.addMark(el));
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+}
