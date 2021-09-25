bae7d810969bb802acee467eddb78b86788a8757
“Yuri <yura.kopshev@gmail.com>
2021-09-25 16:44:19 +0400

    solved task1 in base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let discriminant = b**2-4*a*c;
+  let x1;
+  let x2;
+ if(discriminant===0){
+  x1 = -b/(2*a);
+  arr.push(x1);
+ }
+ else if(discriminant>0){
+  x1 = (-b + Math.sqrt(discriminant) )/(2*a);
+  x2 = (-b - Math.sqrt(discriminant) )/(2*a);
+  arr.push(x1,x2);
+ }
   return arr; // array
 }
 
