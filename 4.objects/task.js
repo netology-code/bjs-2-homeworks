5e71b686df2f150004ec7155b3b46e5b75294860
“Yuri <yura.kopshev@gmail.com>
2021-10-05 14:50:57 +0400

    solved tasks in objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...mark) {
+    if (this.marks === undefined) {
+        this.marks = [...mark];
+    } else {
+        this.marks.push(...mark);
+    }
+}
+
+Student.prototype.getAverage = function () {
+    let sum = 0;
+    for (const key of this.marks) {
+        sum += key;
+    }
+    return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
+
