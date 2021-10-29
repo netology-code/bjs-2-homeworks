b3516a61f12b3c7c7a30919c414ba1f53e74ea7b
“Yuri <yura.kopshev@gmail.com>
2021-10-29 16:18:16 +0400

    solved tasks in decorators/task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,44 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+    if (idx !== -1) { // если элемент не найден
+      console.log("Из кэша: " + cache[idx].value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + cache[idx].value;
+    } else {
+      let result = func(...args); // в кэше результата нет - придётся считать
+      cache.push({ hash: hash, value: result }); // добавляем элемент с правильной структурой
+      if (cache.length > 5) {
+        cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый) 
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(f, ms) {
+  let isCooldown = false;
+  return function () {
+    if (isCooldown) return;
+    f.apply(this, arguments);
+    isCooldown = true;
+    setTimeout(() => isCooldown = false, ms);
+  };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let isCooldown = false;
+  let count;
+  return function () {
+    if (isCooldown) return;
+    func.apply(this, arguments);
+    isCooldown = true;
+    setTimeout(() => isCooldown = false, ms);
+    count++;
+  };
 }
+
