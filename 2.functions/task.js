c58c780506744ca172df51394eef7e2b531c62cd
Octopus <octopus.dev@yandex.kz>
2022-03-01 09:23:50 +0600

    task.js completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,61 @@
 // Задание 1
+
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    sum = sum + arr[i];
+  }
 
+  let avg = sum / arr.length;
+  avg = Number(avg.toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
+
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
+  max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  for (let j = 0; j < arrOfArr.length; j++) {
+    if (func(arrOfArr[j]) > max) {
+      max = func(arrOfArr[j]);
+    }
+  }
   return max;
 }
 
 // Задание 3
+
 function worker2(arr) {
-  // Ваш код
+  let max = -Infinity;
+  let min = arr[0];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if(arr[i] <=min) {
+      min = arr[i];
+    }
+  }
+  return (max -min);
 }
