3f1984c9764ee4accb69d12fc1c0471826d1c8a2
Octopus <octopus.dev@yandex.kz>
2022-03-02 11:03:03 +0600

    task.js completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
+ 
+  return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])
 
-  // Ваш код
-
-  return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  
+  return arr.filter(item => item % 3 == 0).filter(item => item > 0).map(item => item * 10);
 
-  // Ваш код
-
-  return resultArr; // array
 }
