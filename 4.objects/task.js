4f7938779c8452690485f42f183c3dbfd7dd3a66
Octopus <octopus.dev@yandex.kz>
2022-03-09 21:07:37 +0600

    task.js completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,49 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let alex = new Student ("Алексей", "мужской", 35);
+let cris = new Student ("Кристина", "женский", 28);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject=subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){
+    this.marks = [mark];
+    } else {
+    this.marks.push(mark);
+    }
+    
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){
+    this.marks = marks;
+    } else {
+    this.marks = this.marks.concat(...marks);
+    }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  if(this.marks === undefined){
+    return "Оценки отсутствуют"
+  }else{
+  for(i=0; i<this.marks.length; i++){
+    sum += this.marks[i];
+  }
+  this.average = sum/this.marks.length;
+  return this.average;
+    }}
+
+
+
+Student.prototype.exclude = function(reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded=reason;
+  }
