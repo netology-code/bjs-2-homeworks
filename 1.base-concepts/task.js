9ec139ad2ef337e86e48372db8e42b5a3a7c44ae
unknown <evshkryabiy@google.com>
2022-03-22 16:26:02 +0300

    both tasks for HW1 done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,43 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let root = [];  
+  let noRoot = 'Корней нет';
+  let d = b**2-4*a*c;
+    if (d === 0) {
+        let rootForZero = -b/(2*a);
+        root.push(rootForZero);        
+    }
+    else if (d>0){
+        let rootOne = (-b + Math.sqrt(d) )/(2*a);
+        let rootTwo = (-b - Math.sqrt(d) )/(2*a);
+        root.push(rootOne);    
+        root.push(rootTwo);    
+    }
+    else {
+      return root;
+    }
+  return root;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let timeInMonths = 0;
+  let today = new Date;
+  let yearsFromNow = date.getFullYear() - today.getFullYear();
+  if (yearsFromNow >= 1){
+    timeInMonths = 12 * yearsFromNow;
+  } else {
+    return timeInMonths;
+  }
+  let monthsFromNow = date.getMonth() - today.getMonth();
+  timeInMonths = timeInMonths + monthsFromNow;
+  let percentMonthly = percent/100/12; 
+  let amountToReturn = amount - contribution;
+  //S * (P + (P / (((1 + P)^n) - 1))), где: S - тело кредита,
+  //P - 1/12 процентной ставки (от 0 до 1), n - количество месяцев ^ - возведение в степень
+  let paymentMonthly = amountToReturn * (percentMonthly + (percentMonthly / (((1 + percentMonthly)**timeInMonths)-1)))
+  totalAmount = paymentMonthly * timeInMonths;
+  console.log(totalAmount.toFixed(2))
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  return totalAmount.toFixed(2);
 }
