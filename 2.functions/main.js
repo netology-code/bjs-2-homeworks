a881a8cabb455aa6f9d731d24202a06ec6483d31
unknown <evshkryabiy@google.com>
2022-03-23 13:03:26 +0300

    HW2 all 3 tasks done

--- 2.functions/main.js
+++ 2.functions/main.js
@@ -5,3 +5,6 @@ console.log(getArrayParams([1, -4, 7]));
 console.log(makeWork([[10,10,20],[20,19,10]],worker));
 console.log(makeWork([[12,23,31],[41,5,61]],worker));
 console.log(makeWork([[14,21,36],[15,26,39]],worker));
+
+console.log(makeWork([[-10, -20, -40], [10, 20, 30]],worker2));
+console.log(makeWork([[0, 0, 0], [-1, -99]],worker2));
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,76 @@
+"use strict"
 // Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+const arrMinMax = [];
+for (let i = -100; i <= 100; i++){
+  arrMinMax.push(i)
+}
+function getArrayParams(numbers) {
+  let min = numbers[0];
+  let max = numbers[0];
+  let sum = 0;  
+  for (let i = 0; i <numbers.length; i++){
+    let element = numbers[i];
+    if (element < min){
+      min = element;
+    }
+    if (element>max){
+      max = element;
+    }
+    sum = sum + element;
+  }
+  let avg = sum / numbers.length;
+  avg = Number(avg.toFixed(2));
   return { min: min, max: max, avg: avg };
 }
+const result = getArrayParams(arrMinMax);
+console.log (result);
 
 // Задание 2
+let arrOfArr=[];
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i <arr.length; i++){
+    let element = arr[i];
+    sum = sum + element;
+  }
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
+function makeWork(arrOfArr, worker) {
+  let max = 0;
+  let arrayOfSums = [];
+  for (let i = 0; i <arrOfArr.length; i++){
+    let sumElement = worker(arrOfArr[i]);
+    arrayOfSums.push(sumElement);
+  }  
+  for (let i = 0; i <arrayOfSums.length; i++){
+    let sumInArray = arrayOfSums[i];
+    if (sumInArray > max){
+      max = sumInArray;
+    }
+  }
 
-  // Ваш кода
-  // for ...
-  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  let difference = 0;  
+  for (let i = 0; i <arr.length; i++){
+    let element = arr[i];
+    if (element < min){
+      min = element;
+    }
+    if (element>max){
+      max = element;
+    }    
+  }
+  difference = max - min;
+  difference = Math.abs(difference);
+  return difference;
 }
+
+
+
