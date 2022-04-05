cd03a4f493f1a07db3a63179a08aa5595590c4e4
Сенина Юлия Александровна <y.senina@ens.ru>
2022-04-05 11:45:59 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,38 @@
+"use strict";
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  const d = b**2 - 4*a*c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    let x = (-b/(2*a));
+    arr = [x];
+  } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2*a);
+    let x2 = (-b - Math.sqrt(d)) / (2*a);
+    arr = [x1, x2];
+  };
+  return arr;
+};
 
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let S = amount - contribution;
+  let P = (1/12)*(percent/100);
+  let currentDate;
+  let n = (date - currentDate) / 12;
+  let pay = S * (P + (P / (((1 + P)**n) - 1)));
+  totalAmount = (pay*period).toFixed(2);
+  console.log(Number(totalAmount));
 
-  // код для задачи №2 писать здесь
+  if (percent != Number) {
+    console.log("Параметр percent содержит неправильное значение ${percent}");
+  } else if (contribution != Number) {
+    console.log("Параметр contribution содержит неправильное значение ${contribution}");
+  } else if (amount != Number) {
+    console.log("Параметр amount содержит неправильное значение ${amount}");
+  };
 
   return totalAmount;
 }
