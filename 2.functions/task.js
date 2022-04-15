1271adb5ae4a350aeb788bc9617613a06c0b0186
Сенина Юлия Александровна <y.senina@ens.ru>
2022-04-15 10:58:52 +0300

    second commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,8 +4,9 @@ function getArrayParams(arr) {
   min = arr[0];
   max = arr[0];
   sum = 0;
-
+  
   for (i = 0; i < arr.length; i++) {
+
     if (max < arr[i]) {
       max = arr[i];
     };
@@ -14,31 +15,37 @@ function getArrayParams(arr) {
     };
 
     sum += arr[i];
-    avg = Number((sum / arr.length).toFixed(2));
   };
 
-  return `{ min: ${min}, max: ${max}, avg: ${avg} }`;
+  avg = Number((sum / arr.length).toFixed(2));
+
+  return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum = 0;
+  
   for (i = 0; i < arr.length; i++) {
-  sum += arr[i];
-};
+    sum += arr[i];
+  };
+
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max = arrOfArr[0];
+  let max = func(arrOfArr[0]);
+  arrOfArr = [];
+
   for (i = 0; i < arrOfArr.length; i++) {
-    worker(arrOfArr);
-  if (max < worker(arrOfArr)) {
-    max = sum;
+    if (max < func(arrOfArr[i])) {
+      max = func(arrOfArr[i]);
+    };
   };
-}
+
   return max;
 }
+
 
 // Задание 3
 function worker2(arr) {
