b82be2548f1d40c0d1e705bf15b5b31855358924
AlenEvtikhov <50318546+AlenEvtikhov@users.noreply.github.com>
2022-04-18 16:51:55 +0300

    homework arrays completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
-function compareArrays(arr1, arr2) {
-  let result;
+//Задача 1
 
-  // Ваш код
+function compareArrays(arr1, arr2) {
+  let result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 
   return result; // boolean
 }
 
-function advancedFilter(arr) {
-  let resultArr;
+// Задача 2
 
-  // Ваш код
+function advancedFilter(arr) {
+  // let resultArr = arr.filter(n, i > 0).filter(n, i % 3 === 0).map(n, i * 10);
+  let resultArr = arr.filter((n, i) => n > 0).filter((n, i) => n % 3 === 0).map((n, i) => n * 10);
 
   return resultArr; // array
 }
