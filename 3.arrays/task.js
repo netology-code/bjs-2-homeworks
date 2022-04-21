e799b5db4c0b13f2c3590655db47fbbf096c3588
Юлия Сенина <JuLKa9449@yandex.ru>
2022-04-21 14:36:51 +0300

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
+  const compareNumber = (arr1, arr2) => arr1.every(item) === arr2.every(item);
+    if (arr1.length === arr2.length && compareNumber) {
+      result = true;
+    } else {result = false};
 
-  // Ваш код
-
-  return result; // boolean
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
+ 
+  return resultArr;
 }
