40e28f012147006109bb21e8a326682b2979b15d
Юлия Сенина <JuLKa9449@yandex.ru>
2022-04-22 16:40:46 +0300

    second commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,16 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  const compareNumber = (arr1, arr2) => arr1.every(item) === arr2.every(item);
-    if (arr1.length === arr2.length && compareNumber) {
-      result = true;
-    } else {result = false};
-
-  return result;
+  return result = arr1.length === arr2.length && arr1.every((number, index) => number === arr2[index]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
- 
-  return resultArr;
+  return resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 }
