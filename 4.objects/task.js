a122a795cee67e5e74a1f24a9278d66643eea232
Сенина Юлия Александровна <y.senina@ens.ru>
2022-05-05 12:49:50 +0300

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
-
-}
+  let student1 = new Student("Tony", "male", 37);
+  let student2 = new Student("Buzz", "female", 35);
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  let subject = new setSubject("");
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  mark = new addMark(Number);
+  let marks = [];
+  if (this.marks === undefined) { 
+    mark = this.marks;
+  } else {
+    this.marks.push(mark);
+  };
+};
+
+Student.prototype.addMark = function (...mark) {
+  return `marks: ${marks}`;
+};
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let i = 0; i < length.marks; i++) {   
+    sum += marks[i];
+  };
+  return Number((sum / marks.length).toFixed(2));
+};
+
+Student.prototype.exclude = function (reason) {
+  delete Student.prototype.setSubject, Student.prototype.addMark;
+  this.exclude = "reason";
+};
