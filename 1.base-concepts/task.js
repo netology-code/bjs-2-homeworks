064b19361c3fd61fbaee51edb35510980dafc8d1
arrowls <babuyoff@gmail.com>
2022-05-12 11:12:47 +0300

    done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,20 @@
+'use strict'
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = new Array;
+  const total = b ** 2 - 4 * a * c;
+  if (total < 0) {
+    return [];
+  }
+  if (total == 0) {
+    const temp = -b/(2*a);
+    arr.push(temp)
+  }
+  if (total > 0) {
+    let temp = (-b + Math.sqrt(total) )/(2*a);
+    arr.push(temp);
+    temp = (-b - Math.sqrt(total) )/(2*a);
+    arr.push(temp);
+  }
   return arr; // array
 }
 
