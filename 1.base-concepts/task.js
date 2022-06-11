3ebe12b033e509439fff820ee790e4058bdfcaff
zzzlukaaa <merlin19960403@mail.ru>
2022-06-11 19:53:29 +0500

    dz

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,83 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+
+  let arr=[];
+  let x1,x2;
+  if (a===0) {throw new Error("ошибка")}
+  
+
+  let d=b**2-4*a*c;
+  
+ 
+ if (d===0) {
+  x1=-b/(2*a);
+ arr.push(x1);
+
+  } else if(d>0){
+    x1=(-b + Math.sqrt(d))/(2*a);
+    x2=(-b - Math.sqrt(d))/(2*a);
+    
+   arr.push(x1, x2);
+  }
+  return arr;
 }
+ 
+
+ 
+
+//+ Активируйте строгий режим соответствия.
+//Проконтролируйте корректность введенных данных.
+//+ Посчитайте тело кредита: сумма, которую необходимо вернуть банку (сумма кредита минус первоначальный взнос).
+//+Посчитайте на какой срок был выдан кредит (в месяцах).
+//*Ежемесячная оплата рассчитывается по формуле: Платеж = S * (P + (P / (((1 + P)^n) - 1))), где: S - тело кредита, P - 1/12 процентной ставки (от 0 до 1), n - количество месяцев ^ - возведение в степень
+//Посчитайте общую сумму, которую придется заплатить клиенту.
+//Округлите результат до двух значений после запятой.
+//Выведите результат в консоль, а также верните его из функции. Результатом функции должно быть значение числового типа.
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+
+  //S - тело кредита, 
+  //P - 1/12 процентной ставки (от 0 до 1), 
+  //n - количество месяцев ^ - возведение в степень
+ 
+  
+  //percent- прцентная ставка
+  //contribution- первоначальный взнос
+  // amount общая стоимость - сумма кредита
+  // date срок ипотеки
+  //totalAmount - итого
+
+let P=percent*(1/12)/100;
+let S=amount-contribution;
+
+let nowDate=new Date();
+
+
+
+let year1=nowDate.getFullYear();
+let year2=date.getFullYear();
+let month1=nowDate.getMonth();
+let month2=date.getMonth();
+if(month1===0){
+  month1++;
+  month2++;
+}
+
+
+let n = (year2 - year1) * 12 + (month2 - month1) - 1;
+
+
+let amountPerMonth = S * (P + (P / (((1 + P)**n) - 1)));
+
+totalAmount=(amountPerMonth * n).toFixed(2);
+
+
+
 
   return totalAmount;
 }
