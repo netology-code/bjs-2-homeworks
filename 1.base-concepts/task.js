956f6ad4e9d2c2771091a3b35cb2d5be16d7b96a
zzzlukaaa <merlin19960403@mail.ru>
2022-06-13 11:30:33 +0500

    dz

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -24,6 +24,35 @@ function solveEquation(a, b, c) {
 }
  
 
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  
+  if(typeof percent !== 'number'){return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`}
+  if(typeof contribution !== 'number'){return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`}
+  if(typeof amount !== 'number'){return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`}
+
+    let totalAmount;
+    let S=amount-contribution;
+    
+    let nowDate=new Date();
+      let year1=nowDate.getFullYear();
+      let year2=date.getFullYear();
+      let month1=nowDate.getMonth();
+      let month2=date.getMonth();
+     if(month1===0){
+        month1++;
+        month2++;
+      }
+    let n = (year2 - year1) * 12 + (month2 - month1);
+
+    let P=percent/12/100;
+    
+    let amountPerMonth = S * (P + (P / (((1 + P)**n) - 1)));
+
+    totalAmount=(amountPerMonth * n);
+    totalAmount=Number(totalAmount.toFixed(2));
+  return totalAmount;
+}
  
 
 //+ Активируйте строгий режим соответствия.
@@ -34,50 +63,15 @@ function solveEquation(a, b, c) {
 //Посчитайте общую сумму, которую придется заплатить клиенту.
 //Округлите результат до двух значений после запятой.
 //Выведите результат в консоль, а также верните его из функции. Результатом функции должно быть значение числового типа.
+// код для задачи №2 писать здесь
 
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  //S - тело кредита, 
-  //P - 1/12 процентной ставки (от 0 до 1), 
-  //n - количество месяцев ^ - возведение в степень
+    //S - тело кредита, 
+    //P - 1/12 процентной ставки (от 0 до 1), 
+    //n - количество месяцев ^ - возведение в степень
  
   
-  //percent- прцентная ставка
-  //contribution- первоначальный взнос
-  // amount общая стоимость - сумма кредита
-  // date срок ипотеки
-  //totalAmount - итого
-
-let P=percent*(1/12)/100;
-let S=amount-contribution;
-
-let nowDate=new Date();
-
-
-
-let year1=nowDate.getFullYear();
-let year2=date.getFullYear();
-let month1=nowDate.getMonth();
-let month2=date.getMonth();
-if(month1===0){
-  month1++;
-  month2++;
-}
-
-
-let n = (year2 - year1) * 12 + (month2 - month1) - 1;
-
-
-let amountPerMonth = S * (P + (P / (((1 + P)**n) - 1)));
-
-totalAmount=(amountPerMonth * n).toFixed(2);
-
-
-
-
-  return totalAmount;
-}
+    //percent- прцентная ставка
+    //contribution- первоначальный взнос
+    // amount общая стоимость - сумма кредита
+    // date срок ипотеки
+    //totalAmount - итого
