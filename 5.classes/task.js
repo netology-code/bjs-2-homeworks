775596e39d351a122cdad6a38a676ac9e05477b9
Yuri Kopshev <yura.kopshev@gmail.com>
2022-08-19 19:48:23 +0200

    changed task.js library solution

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -91,17 +91,67 @@ class Library {
         return null;
     }
 
-    giveBookByName(bookName) {
-        let element;
-        for (let i = 0; i < this.books.length; i++) {
-            if (this.books[i].name === bookName) {
-                element = this.books[i];
-                delete this.books[i];
-                this.books = this.books.slice(0, this.books.length - 1);
-                return element;
-            }
-        }
-        return null;
+  giveBookByName(bookName) {
+    let element = []
+    for (let i = 0; i < this.books.length; i++) {
+      if (this.books[i].name === bookName) {
+        element = this.books.splice(i,1);
+        return element[0];
+      }
     }
+    return null;
+  }
+}
+
+
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.map = new Map();
+  }
+
+  addMark(mark, subject) {
+    if (mark > 5) {
+      console.log("Ошибка, оценка должна быть числом от 1 до 5");
+      return;
+    }
+    if (this.map.has(subject)) {
+      this.map.get(subject).push(mark);
+      return;
+    }
+    this.map.set(subject, [mark]);
+  }
+
+  getAverageBySubject(subject) {
+    if (!this.map.has(subject)) {
+      console.log("Несуществующий предмет");
+      return;
+    }
+    let avvSum = 0;
+    let arr = new Array();
+    arr = this.map.get(subject);
+    for (let i = 0; i < arr.length; i++) {
+      avvSum += arr[i];
+    }
+    return avvSum / arr.length;
+  }
+
+  getAverage() {
+    let arr = [];
+    let count = 0;
+    let sum = 0;
+    arr = this.map.values();
+    for (const value of arr) {
+      for (let i = 0; i < value.length; i++) {
+        sum += value[i]; 3
+        count++;
+      }
+    }
+    return sum / count;
+  }
+
+  exclude() {
+    console.log("Исключен за попытку подделать оценки");
+  }
 }
 
