5ddda0075530c8fc41ea2cbd8287220c8ef31f3c
Yuri Kopshev <yura.kopshev@gmail.com>
2022-09-02 09:14:33 +0200

    changed compareArrays()

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,11 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-  result = arr1.every(number => arr1[number] === arr2[number]);
+  result =
+    arr1.length === arr2.length &&
+    arr1.every((number) => arr1[number] === arr2[number]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-  resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number*10);
+  resultArr = arr
+    .filter((number) => number > 0)
+    .filter((number) => number % 3 === 0)
+    .map((number) => number * 10);
   return resultArr; // array
 }
