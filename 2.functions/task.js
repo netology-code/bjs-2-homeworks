635183ff437e9a2aed225c6c3f67c3c5b465932a
boarderbare <boarderbare@gmail.com>
2022-09-19 17:09:10 +0400

    dz2_functions done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,49 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let avg;
+  let sum = 0;
 
-  // Ваш код
+  for (let k = 0; k < arr.length; k++) {
+    if (arr[k] < min) {
+      min = arr[k];
+    }
+    if (arr[k] > max) {
+      max = arr[k];
+    }
+    sum += arr[k];
+  }
 
-  return { min: min, max: max, avg: avg };
+  avg = (sum / arr.length).toFixed(2);
+  return { min: min, max: max, avg: +avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
-
+  for (j = 0; j < arr.length; j++) {
+    sum += arr[j];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let maxArr;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  for (i = 0; i < arrOfArr.length; i++) {
+    let length = arrOfArr.length;
+    maxArr = func(arrOfArr[i]);
+    if (maxArr > max) {
+      max = maxArr;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  return Math.abs(getArrayParams(arr).max - getArrayParams(arr).min);
 }
