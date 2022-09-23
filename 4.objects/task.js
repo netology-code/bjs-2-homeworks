9173b841930c51979df4d970f2c80e8c8c1773b8
boarderbare <boarderbare@gmail.com>
2022-09-23 16:37:46 +0400

    dz4 done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...array) {
+  if (this.marks === undefined) {
+    this.marks = [...array];
+  } else {
+    this.marks.push(...array);
+  }
+}
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach(item => sum = sum + item);
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
