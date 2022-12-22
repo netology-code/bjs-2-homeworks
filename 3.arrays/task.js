023952b5d4591ca672675e67d3c6dc6a8704c841
alexandrzapevalov <avzapevalov@gmail.com>
2022-12-22 06:02:46 +0000

    3 Array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,7 @@ function compareArrays(arr1, arr2) {
   return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
   let result = users.filter(user => user.gender === gender).map(itemAges => itemAges.age).reduce((sumAges, age, index, arrAges) => {
     sumAges += age;
