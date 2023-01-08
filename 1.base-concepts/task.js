c7e41fa5fceba3207d7ad46eaa1e44192c5f2216
zenitfan88 <nikanenkov@rambler.ru>
2023-01-08 13:09:59 +0400

    init commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)); 
+  };
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (typeof (percent) !== 'number' || typeof (contribution) !== 'number' || typeof (amount) !== 'number') {
+    return false;
+  };
+
+  let differenceAmount = amount - contribution;
+  let monthPercent = percent / (12 * 100);
   
+  let paymentMounth = differenceAmount * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+  let amountReturn = Number((countMonths * paymentMounth).toFixed(2));
+
+  return amountReturn;
 }
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,98 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= max) {
+      max = arr[i];
+    } else if (arr[i] <= min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  };
+
+  let avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
+  let sum = 0;
 
-}
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  };
+  
+  return sum;
+};
 
 function differenceMaxMinWorker(...arr) {
+  let min = arr[0];
+  let max = arr[0];
 
-}
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] >= max) {
+        max = arr[i];
+      } else if (arr[i] <= min) {
+        min = arr[i];
+      }
+    }
+  } else {
+    min = 0;
+    max = 0;
+  };
+
+  let difference = max - min;
+  return difference;
+};
 
 function differenceEvenOddWorker(...arr) {
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
 
-}
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  };
+
+  let differenceSumEvenAndOdd = sumEvenElement - sumOddElement;
+  return differenceSumEvenAndOdd;
+};
 
 function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  let averageEvenElement = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement += 1; 
+    }
+  };
+ 
+  if (arr.length > 0) {
+    averageEvenElement = Number((sumEvenElement / countEvenElement).toFixed(2));
+  } else {
+    averageEvenElement = 0;
+  };
 
-}
+  return averageEvenElement;
+};
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = - Infinity;
 
-}
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let comparedValue = func(...arrOfArr[i]);
+    if (comparedValue > maxWorkerResult) {
+      maxWorkerResult = comparedValue;
+    }
+  };
+
+  return maxWorkerResult;
+};
