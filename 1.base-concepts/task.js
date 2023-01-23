348c648878149ecdad4f28931bffda0413ac0db4
Дарья Фокина <cool.russia-33@yandex.ru>
2023-01-23 22:50:34 +0300

    add  colution of the task by discrminant

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,28 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
+"use strict";
+function solveEquation(a,b,c) {
+  let arr=[];
+  let d = (b**2-4*a*c);
+
+  if (d>0){
+   let root1 = (-b + Math.sqrt(d) )/(2*a);
+   let root2 =  (-b - Math.sqrt(d) )/(2*a);
+    arr.push(root1,root2);
+    console.log(arr);
+    return arr;
+  }
+
+if (d === 0){
+  let root0= -b/(2*a);
+  arr.push(root0);
+    console.log(arr);
   return arr;
+}
+
+else {
+  console.log("корней нет"+ arr)
+}
+
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
