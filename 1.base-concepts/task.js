f1334c73fe330c867c6d7bc999df2a2e4dbe9a2c
Сергей <vaskovsergey95@gmail.com>
2023-02-06 00:42:15 +0500

    correction

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,19 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+"use strict";
+
+function solveEquation(a, b ,c) {
+  let arr = [];
+  let d = b ** 2 - 4* a * c;
+
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  }
+  else if (d === 0) {
+    let x3 = d - b/(2*a);
+    arr.push(x3);
+  }
+  return arr;
 }
+console.log(solveEquation(1, 2, -10));
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
