9700fab00a12ed636d96e61637b6bdf8b9a4464c
Leferchian <leferchian@gmail.com>
2023-02-19 17:05:59 +0300

    JS First Lesson

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = (b ** 2) - (4 * a * c);
+  if (d > 0) {
+  arr.push((-b + Math.sqrt(d)) / (2 * a));
+  arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
+  else if (d == 0) {
+  arr.push(-b / (2 * a));
+  }
+  
+  
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
