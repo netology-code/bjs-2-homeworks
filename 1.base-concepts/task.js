871d63c9b07f5d4ec5364394ccc79c364d12d0b4
Egor Korchenkin <egorkorchenkin@yandex.ru>
2023-02-19 13:59:33 +0300

    solve 1.base-concepts/task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const d = b**2-4*a*c;
+  if (d < 0){
+    arr = [];
+  }
+  if (d == 0){
+    const x = -b/(2*a);
+    arr = [x];
+  }
+  if (d > 0){
+    const x = (-b + Math.sqrt(d))/(2*a);
+    const xTwo = (-b - Math.sqrt(d))/(2*a);
+    arr = [x,xTwo];
+  }
   return arr;
 }
 
