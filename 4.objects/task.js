11acce3f5815072ca5badb1186fd4f8916bf7128
vadimnikolaevich <124394236+vadimnikolaevich@users.noreply.github.com>
2023-02-20 16:54:30 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks !== undefined) {
+        this.marks.push(...marks);
+      }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined || this.marks.length === 0) {
+        return 0;
+      }
+      let sum = 0;
+      for (let index = 0; index < this.marks.length; index++) {
+        sum += this.marks[index];
+      }
+      return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
