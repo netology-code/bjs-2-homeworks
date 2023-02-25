86fbc0d5163b991a28c41ad656bcd0628c82243e
Leferchian <leferchian@gmail.com>
2023-02-25 20:51:07 +0300

    JS lesson Array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);;
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(number => number > 0).filter(number => number % 3 == 0).map((number) => number * 10);
 
   return resultArr; // array
 }
