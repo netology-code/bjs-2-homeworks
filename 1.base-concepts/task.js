d682f1aca988279fb41d287255ea3bd9980dbaa9
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-03-20 21:41:47 +0300

    task.js modified

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+  const discr = b ** 2 - 4 * a * c;
   
+  if (discr > 0) {
+    arr = [ (-b + Math.sqrt(discr))/(2 * a), (-b - Math.sqrt(discr))/(2 * a) ];
+  } else if (discr == 0) {
+      arr = [ -b / (2 * a) ];
+  }
+
   return arr;
 }
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(+percent) || isNaN(+contribution) || isNaN(+amount) || isNaN(countMonths)) {
+      return false;
+  }
+
+  const percentMonths = percent / 1200;
+  const bodyCredit = amount - contribution;
+  const montlyPayment = bodyCredit * (percentMonths + (percentMonths / ((( 1 + percentMonths) ** countMonths) - 1))); 
+
+  const total = montlyPayment * countMonths;
+  return +total.toFixed(2);
 }
