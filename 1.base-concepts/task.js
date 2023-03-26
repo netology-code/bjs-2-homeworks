41947de7d6ac1e5dbbb363af3c68dd956e242f3c
SenSeTioN <lankovedik@gmail.com>
2023-03-26 19:42:53 +0300

    Change task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,26 +1,28 @@
 'use strict';
 
 function solveEquation(a, b, c) {
-	let arr = [];
+	const arr = [];
 	const discriminant = b ** 2 - 4 * a * c;
 
-	if (discriminant < 0) {
-		arr = [];
-	} else if (discriminant === 0) {
-		arr = [-b / (2 * a)];
-	} else {
-		arr = [
+	if (discriminant === 0) {
+		arr.push(-b / (2 * a));
+	} else if (discriminant > 0) {
+		arr.push(
 			(-b + Math.sqrt(discriminant)) / (2 * a),
-			(-b - Math.sqrt(discriminant)) / (2 * a),
-		];
+			(-b - Math.sqrt(discriminant)) / (2 * a)
+		);
 	}
 	return arr;
 }
 
+console.log(solveEquation(1, 2, 1));
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	const percentStage = (percent / 100) / 12;
+	const percentStage = percent / 100 / 12;
 	const bodyOfCredit = amount - contribution;
-	const payment = bodyOfCredit * (percentStage + (percentStage / (((1 + percentStage) ** countMonths) - 1)));
+	const payment =
+		bodyOfCredit *
+		(percentStage + percentStage / ((1 + percentStage) ** countMonths - 1));
 	const totalAmount = +(payment * countMonths).toFixed(2);
 
 	if (Number.isNaN(totalAmount)) {
@@ -30,4 +32,4 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	return totalAmount;
 }
 
-console.log(calculateTotalMortgage(10, 0, 50000, 12));
+console.log(calculateTotalMortgage(10, 1000, 50000, 12));
