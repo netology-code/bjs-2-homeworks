1024fa6ee272518c1019fb3f3de70382ce85db9d
SenSeTioN <lankovedik@gmail.com>
2023-04-04 18:25:53 +0300

    delete console.log

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -8,6 +8,7 @@ function getArrayParams(...arr) {
 
 	return { min, max, avg };
 }
+
 function summElementsWorker(...arr) {
 	if (!arr || arr.length === 0) {
 		return 0;
@@ -74,14 +75,3 @@ function makeWork(arrOfArr, func) {
 	}
 	return maxWorkerResult;
 }
-
-const arr = [
-	[10, 10, 11, 20, 10],
-	[67, 10, 2, 39, 88],
-	[72, 75, 51, 87, 43],
-	[30, 41, 55, 96, 62],
-];
-console.log(makeWork(arr, summElementsWorker)); // максимум из 61, 206, 328, 284 => 328
-console.log(makeWork(arr, differenceMaxMinWorker)); // максимум из 10, 86, 44, 66 => 86
-console.log(makeWork(arr, differenceEvenOddWorker)); // максимум из 39, -6, -184, 92 => 92
-console.log(makeWork(arr, averageEvenElementsWorker)); // максимум из 12.5, 33.333, 72, 62.666 => 72
