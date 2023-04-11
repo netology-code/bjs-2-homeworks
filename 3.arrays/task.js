45109632b9deca9d22eb5925b33eb99797324567
SenSeTioN <lankovedik@gmail.com>
2023-04-12 00:45:57 +0300

    Complete homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((el, i) => el === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	if (
+		!Array.isArray(users) ||
+		users.length === 0 ||
+		typeof gender !== 'string'
+	) {
+		return 0;
+	}
+
+	const filteredUsers = users.filter(user => user.gender === gender);
+	if (filteredUsers.length === 0) {
+		return 0;
+	}
+
+	const totalAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+	const averageAge = totalAge / filteredUsers.length;
+
+	return averageAge;
 }
