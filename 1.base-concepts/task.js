26260fa8291e5f2793dd10ecbbaa4d226ce42c4a
ELETRAUD <122927068+ELETRAUD@users.noreply.github.com>
2023-04-13 15:51:11 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,32 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  const discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) {
+    return [];
+  } else if (discriminant === 0) {
+    const x = -b / (2 * a);
+    return [x];
+  } else {
+    const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [x1, x2];
+  }
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let totalAmount = 0;
+  const percentPerMonth = parseFloat((percent / 100) / 12);
+  const creditAmount = parseFloat(amount - contribution);
+ 
+  if (isNaN(parseFloat(percent)) || isNaN(parseFloat(contribution)) || isNaN(parseFloat(amount)) || isNaN(parseFloat(countMonths))) {
+    return false;
+  } else {
+    const monthlyPayment = parseFloat(creditAmount  (percentPerMonth + percentPerMonth / (Math.pow(1 + percentPerMonth, countMonths) - 1)));
+    totalAmount = parseFloat(monthlyPayment  countMonths);
+    totalAmount = parseFloat(totalAmount.toFixed(2));
+    return totalAmount;
+  }
 }
