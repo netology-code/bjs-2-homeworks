06f3d0a982a9cf314e112ab77cefa27638a87f56
SenSeTioN <lankovedik@gmail.com>
2023-04-18 01:25:13 +0300

    Удалил примеры

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -32,16 +32,3 @@ Student.prototype.exclude = function (reason) {
 		this.excluded = reason;
 	}
 };
-
-let student1 = new Student('Василиса', 'женский', 19);
-student1.setSubject('Algebra');
-console.log(student1.getAverage()); // 0
-student1.addMarks(4, 5, 4, 5);
-console.log(student1.getAverage()); // 4.5
-console.log(student1);
-// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
-let student2 = new Student('Артём', 'мужской', 25);
-student2.setSubject('Geometry');
-student2.exclude('плохая учёба');
-console.log(student2);
-// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
