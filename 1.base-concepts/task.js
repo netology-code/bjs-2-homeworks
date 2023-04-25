b7799a825e64e13746c6eadecf5199d14be5071e
Archi1313 <13archi@mail.ru>
2023-04-25 22:36:36 +0300

    hope it is done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let discrim=b**2-4*a*c;
+  if (discrim==0) {
+    arr[1]=-b/(2*a);
+  } else if (discrim>0) {
+    arr[1]=(-b + Math.sqrt(discrim) )/(2*a);
+    arr[2]=(-b - Math.sqrt(discrim) )/(2*a);
+  }
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percentMounth=percent/1200;
+  creditBody=amount-contribution;
+  mounthPay=creditBody * (percentMounth + (percentMounth / (((1 + percentMounth)**countMonths) - 1)));
+  summToPay=contribution+creditBody+mounthPay*countMonths;
+  return summToPay;
 }
