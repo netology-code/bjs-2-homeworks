39510d491fcc48a95aec7cc4c5e3f5ee9fb64f46
Angelina Korotkova <angelinakorotkova0401@mail.ru>
2023-04-28 17:53:00 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,59 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+
+  if(a === 0) {
+   alert('Ошибка, a = 0 недостимое значение!');
+   return;
+ }
+let d = b*b - 4 * a * c;
+let x1, x2;
+
+ if(d < 0) return [];
+ 
+  if(d === 0) {
+   return [
+   x1 = -b / 2 * a
+  ];
+ }
+ 
+ return [
+   x1 = (-b + Math.sqrt(d)) / (2 * a),
+   x2 = (-b - Math.sqrt(d)) / (2 * a),
+ ];
 }
+
+console.log(solveEquation(1,6,1));
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+  let rate,
+      ratePerMonth,
+      loanBody,
+      payment,
+      totalLoanAmount;
+
+    // ставка в диапазоне от 0 до 1
+   rate = percent / 100;
+  
+    // ставка в месяц 
+   ratePerMonth = rate / 12;
+
+   // тело кредита
+   loanBody = amount - contribution;
+
+    // ежемесячная оплата
+   payment = loanBody * (ratePerMonth + (ratePerMonth / (((1 + ratePerMonth) ** countMonths) - 1)));
+  
+   //итоговая сумма кредита 
+   totalLoanAmount = + (payment * countMonths).toFixed(2);
+  
+   console.log('Итоговая сумма кредита: ' + totalLoanAmount);
+  
+   return totalLoanAmount;
 }
+
+calculateTotalMortgage(15, 0, 10000, 36);
