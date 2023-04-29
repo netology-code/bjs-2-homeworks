7d84529f0060a5a1d17885f205fe57966ecc7ea8
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-04-29 21:45:23 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = b**-4*a*c;
+
+  let arr = [discriminant];
   
+if(discriminant<0){
+  arr=[];
+}
+if(discriminant===0){
   return arr;
 }
-
+}
+solveEquation(1,5,4);
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
