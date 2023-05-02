212b3f230494aa76be22790ccc19ae7d7e59ab4d
ELETRAUD <122927068+ELETRAUD@users.noreply.github.com>
2023-05-02 16:26:30 +0300

    Update task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -75,6 +75,9 @@ class Library {
   }
 
   findBookBy(type, value) {
+    if (this.books.length === 0) {
+      return null;
+    }
     for (let book of this.books) {
       if (book[type] === value) {
         return book;
@@ -84,105 +87,9 @@ class Library {
   }
 
   giveBookByName(bookName) {
-    for (let i = 0; i < this.books.length; i++) {
-      if (this.books[i].name === bookName) {
-        return this.books.splice(i, 1)[0];
-      }
+    if (this.books.length === 0) {
+      return null;
     }
-    return null;
-  }
-}
-
-
-
-2//
-
-class PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
-    this.name = name;
-    this.releaseDate = releaseDate;
-    this.pagesCount = pagesCount;
-    this.state = 100;
-    this.type = null;
-  }
-
-  fix() {
-    this.state *= 1.5;
-  }
-
-  set state(newState) {
-    if (newState < 0) {
-      this._state = 0;
-    } else if (newState > 100) {
-      this._state = 100;
-    } else {
-      this._state = newState;
-    }
-  }
-
-  get state() {
-    return this._state;
-  }
-}
-
-class Magazine extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.type = "magazine";
-  }
-}
-
-class Book extends PrintEditionItem {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.type = "book";
-    this.author = author;
-  }
-}
-
-class NovelBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = "novel";
-  }
-}
-
-class FantasticBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = "fantastic";
-  }
-}
-
-class DetectiveBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = "detective";
-  }
-}
-
-class Library {
-  constructor(name) {
-    this.name = name;
-    this.books = [];
-  }
-
-  addBook(book) {
-    if (book.state > 30) {
-      this.books.push(book);
-    }
-  }
-
-  findBookBy(type, value) {
-    for (let book of this.books) {
-      if (book[type] === value) {
-        return book;
-      }
-    }
-    return null;
-  }
-
-  giveBookByName(bookName) {
     for (let i = 0; i < this.books.length; i++) {
       if (this.books[i].name === bookName) {
         return this.books.splice(i, 1)[0];
@@ -193,38 +100,3 @@ class Library {
 }
 
 const library = new Library("Национальная библиотека");
-
-library.addBook(new NovelBook(
-  "Харуки Мураками",
-  "1Q84",
-  2009,
-  1312
-));
-library.addBook(new DetectiveBook(
-  "Агата Кристи",
-  "Десять негритят",
-  1939,
-  256
-));
-library.addBook(new Magazine(
-  "National Geographic",
-  1888,
-  200
-));
-
-// Найдем книгу, изданную в 1919 году, и если ее нет, то создадим
-let book1919 = library.findBookBy("releaseDate", 1919);
-if (!book1919) {
-  book1919 = new PrintEditionItem("Название книги", 1919, 100);
-  library.addBook(book1919);
-}
-
-// Выдадим любую книгу и повредим ее состояние
-const bookToGive = library.giveBookByName("1Q84");
-bookToGive.state = 20;
-
-// Восстановим состояние книги и попытаемся добавить ее обратно в библиотеку
-bookToGive.fix();
-library.addBook(bookToGive);
-
-console.log(library.books); // [NovelBook, DetectiveBook, Magazine, PrintEditionItem]
