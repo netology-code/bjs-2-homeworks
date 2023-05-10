91d9bfd32c41cfe16d9da1e4097956c085b3f32d
anastasiynovaya <aefremova3014@gmail.com>
2023-05-10 21:06:23 +0500

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,11 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+    return false;
+}
+return arr1.every((value, index) => 
+{console.log(value, arr2[index]);
+return value === arr2[index];
+});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
