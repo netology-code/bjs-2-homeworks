0ba5a9aaf4fcfa2ae53b5c5e3e7053f3f7399377
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-05-17 20:33:16 +0300

    1
    1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,38 @@
 "use strict"
+"use strict"
 function solveEquation(a, b, c) {
-  let discriminant = b**-4*a*c;
-
-  let arr = [discriminant];
-  
-if(discriminant<0){
-  arr=[];
-}
-if(discriminant===0){
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  } else if (d == 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  }
+  console.log(arr);
   return arr;
 }
-}
-solveEquation(1,5,4);
+
+solveEquation(1, 2, 8);
+
+// код для задачи №2 писать здесь
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+/* Описание переменных
+percent: процент
+contribution: взнос
+amount: сумма
+countMonths: кол-во месяцев
+BodyCredit: тело кредита
+PayMount: ежемесячный платеж
+*/
+let BodyCredit=amount-contribution;
+let PayMount= BodyCredit * (percent+(percent/(((1+percent)**countMonths)-1)));
+let PayMountRounded=PayMount.toFixed(2);
+console.log(PayMountRounded);
+
+
 }
+calculateTotalMortgage(15,0,10000,36);
