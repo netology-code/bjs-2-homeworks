c763965c2a1e8849779a19f2a47eb38e09d818c5
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-05-24 20:34:21 +0300

    modifed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,41 +1,85 @@
 // Задание 1
-function getArrayParams(arr) {
+function getArrayParams(...arr) {
   let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
   sum = 0;
   min = max = arr[0];
   arr.forEach(element => {
     sum += element;
-    if (min >= element ) {min = element};
-    if (max <= element ) {max = element};
+    if (min >= element) { min = element };
+    if (max <= element) { max = element };
   });
-  avg = Number((sum/arr.length).toFixed(2));
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
-// Задание 2
-function worker(arr) {
-  let sum =0;
-  arr.forEach(element => {sum += element});
+//Задание 2
+function summElementsWorker(...arr) {
+  let sum = 0;
+  arr.forEach(element => { sum += element });
   return sum;
+
 }
 
-function makeWork(arrOfArr, func) {
-  let max =0;
-  arrOfArr.forEach(element => {
-    let tmp = func(element);
-    if (max <= tmp ) {max = tmp};
-  });
-  return max;
-}
-
-// Задание 3
-function worker2(arr) {
+function differenceMaxMinWorker(...arr) {
   let min, max, d;
   min = max = d = arr[0];
   arr.forEach(element => {
-    if (min >= element ) {min = element};
-    if (max <= element ) {max = element};
+    if (min >= element) { min = element };
+    if (max <= element) { max = element };
   });
-  d = max-min;
+  d = max - min;
   return Math.abs(d);
+}
+
+function differenceEvenOddWorker(...arr) {
+  let sumOddElement = 0;//нечетный
+  let sumEvenElement = 0;//четный
+  let d = 0;
+
+  arr.forEach(element => {
+
+    if (element % 2 == 0) {
+      //четный
+      sumEvenElement += element;
+    }
+    else {
+      //Нечётное
+      sumOddElement += element;
+    }
+    d = sumEvenElement - sumOddElement;
+    return Math.abs(d);
+
+  })
+  console.log(d);
+}
+
+function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;//четный
+  let countEvenElement = 0;
+
+  arr.forEach(element => {
+
+    if (element % 2 == 0) {
+      //четный
+      sumEvenElement += element;
+    }
+    else {
+      countEvenElement += element;
+    }
+    countEvenElement = (sumEvenElement + sumEvenElement) / 2;
+    return Math.abs(countEvenElement);
+
+  })
+  console.log(countEvenElement);
+}
+//Задание 3
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  arrOfArr.forEach(element => {
+    let tmp = func(element);
+    if (maxWorkerResult <= tmp) { maxWorkerResult = tmp };
+  });
+  return maxWorkerResult;
 }
