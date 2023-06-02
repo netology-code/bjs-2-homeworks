4b1864aed96543062bee9cafb7f95df6be27e4cd
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-06-02 17:08:59 +0300

    modified

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -17,14 +17,57 @@ Student.prototype.setSubject = function(subjectName) {
 student_1.setSubject("Algebra");
 student_2.setSubject("Fizika");
 
-if(student_1.hasOwnProperty("marks") || student_2.hasOwnProperty("marks")) {
-    Student.prototype.addMarks = function(...marksToAdd) {
+if(student_1.hasOwnProperty("marks")){
+    student_1.addMarks = function(...marksToAdd) {
         this.marks = marksToAdd;
     }
 }
 
-student_1.addMarks(4, 5, 4, 5);
+if(student_2.hasOwnProperty("marks")){
+    student_2.addMarks = function(...marksToAdd) {
+        this.marks = marksToAdd;
+    }
+}
+
+student_1.addMarks(4, 5, 5, 5);
+student_2.addMarks(4, 5, 4, 5);
 
 console.log(student_1);
 console.log(student_2);
+
+if(student_1.hasOwnProperty("marks")) {
+    student_1.getAverage = function() {
+        let sum = 0;
+        if(student_1.marks.length != 0) {
+            for(let i = 0; i < student_1.marks.length; i++) {
+                sum += student_1.marks[i];
+            }
+            console.log(sum);
+        }else {
+            console.log(sum);
+        }
+        
+    }
+
+}
+
+if(student_2.hasOwnProperty("marks")) {
+    student_2.getAverage = function() {
+        let sum = 0;
+        if(student_2.marks.length != 0) {
+            for(let i = 0; i < student_2.marks.length; i++) {
+                sum += student_2.marks[i];
+            }
+            console.log(sum);
+        }else {
+            console.log(sum);
+        }
+        
+    }
+
+}
+
+student_1.getAverage();
+student_2.getAverage();
+
 
