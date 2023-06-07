46abd0937882c9184c30512142b5eee72d0d1f93
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-06-07 15:53:36 +0300

    modifed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -7,22 +7,22 @@ function Student(name, gender, age) {
    this.marks = [];
 }
 
-let tudent1 = new Student("Василиса", "женский", 19);
+let student1 = new Student("Василиса", "женский", 19);
 let student2 = new Student("Артём", "мужской", 25);
 
 
 Student.prototype.setSubject = function(subjectName) {
    this.subject = subjectName;
 }
-tudent1.setSubject("Algebra");
+student1.setSubject("Algebra");
 student2.setSubject("Geometry");
 
 Student.prototype.addMarks = function(...marksToAdd) {
    this.marks = marksToAdd;
 }
 
-if(tudent1.hasOwnProperty("marks")){
-   tudent1.addMarks(4, 5, 4, 5);
+if(student1.hasOwnProperty("marks")){
+   student1.addMarks(4, 5, 4, 5);
 }
 
 if(student2.hasOwnProperty("marks")){
@@ -42,8 +42,8 @@ Student.prototype.getAverage = function() {
 
 }
 
-if(tudent1.hasOwnProperty("marks")) {
-   tudent1.getAverage();
+if(student1.hasOwnProperty("marks")) {
+   student1.getAverage();
 }
 
 if(student2.hasOwnProperty("marks")) {
@@ -57,5 +57,5 @@ Student.prototype.exclude = function(reason) {
 }
 
 student2.exclude("Плохая учеба");
-console.log(tudent1);
+console.log(student1);
 console.log(student2);
