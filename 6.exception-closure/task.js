ceb6c53d86f6af9f434ead1427056ee813495b6b
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-06-08 17:21:03 +0300

    modifed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -63,3 +63,42 @@ function getTriangle(a, b, c) {
     }
 }
 console.log(getTriangle(1, 10, 3));
+/*
+class Triangle {
+   constructor(a, b, c) {
+       this.a = a;
+       this.b = b;
+       this.c = c;
+       this.p = 0;
+       this.s = 0;
+   
+       if (a + b < c || a + c < b || b + c < a) {
+          throw new Error ('Треугольник с такими сторонами не существует');
+       }       
+   }
+
+   
+   
+   getPerimeter() {
+       this.p = (this.a + this.b + this.c)/2;
+       return this.p;
+   }
+   getArea() {
+       this.s = Number(Math.sqrt(this.p * (this.p - this.a) * (this.p - this.b) * (this.p - this.c)).toFixed(3));
+       return this.s;
+
+   }
+}
+
+function getTriangle(a, b, c) {
+   try {
+       return new Triangle(a, b, c);
+   }
+   catch (err) {
+       
+   }
+}
+const triangle = getTriangle(10, 10, 10);
+console.log(triangle.getPerimeter());
+console.log(triangle.getArea());
+*/
