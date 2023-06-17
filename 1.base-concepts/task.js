147461839d29d7c129eada901f484d3e76f98290
you <you@example.com>
2023-06-18 00:27:50 +0300

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,19 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+
+function solveEquation(a,b,c) {
+  let discriminant = Math.pow(b,2) -4*a*c;
+  let answer = [];
+  if (discriminant<0) {
+    console.log("корней нет")
+  }
+  else if(discriminant==0) {
+    answer.push(-b/(2*a));
+  }
+  else {
+    answer.push((-b + Math.sqrt(discriminant))/(2*a));
+    answer.push((-b - Math.sqrt(discriminant))/(2*a));
+  }
+  return answer;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
