05478abb3b255b7c3fc3a7c599253b5af30d5777
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-18 23:12:36 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,26 @@
 function compareArrays(arr1, arr2) {
-  
+  let result;
+  if (arr1.length === arr2.length) {
+    result = arr1.every((item, index) => item === arr2[index]);
+  } else {
+    result = false;
+  }
+  return result; // boolean
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+// function compareArrays(arr1, arr2) {
+//   return arr2.length === arr1.length && arr1.every((item, index) => item === arr2[index]); 
+// }
+
+function advancedFilter(arr) {
+  return arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10)
 }
+
+// function advancedFilter(arr) {
+//   return arr.reduce((acc, value) => {
+//     if (value > 0 && value % 3 === 0) {
+//       acc.push(value * 10)
+//     }
+//     return acc;
+//   }, [])
+// }
