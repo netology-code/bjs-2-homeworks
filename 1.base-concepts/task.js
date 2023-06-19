b9d52e2b0aa2641c18e2839222e3df2ced08d85e
you <you@example.com>
2023-06-19 11:53:13 +0300

    First task calculateTotalMortgage

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,7 @@
 "use strict"
 
 function solveEquation(a,b,c) {
+  if (isNaN(a) || isNaN(b) || isNaN(c)) return false;
   let discriminant = Math.pow(b,2) -4*a*c;
   let answer = [];
   if (discriminant<0) {
@@ -17,27 +18,11 @@ function solveEquation(a,b,c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  //percent процентную ставку, 
-  //contribution сумму первоначального взноса,
-  //amount сумму кредита,
-  //countMonths срок в месяцах
-  // if(typeof percent !="number" || typeof contribution !="number" || typeof amount!="number" || typeof countMonths)
-  // {
-  //   return false;
-  // }
-  percent = percent/100;
-  let mPercent = percent/12;
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) return false;
+  let mPercent = (percent/100)/12;
   let body = amount - contribution;
-  let payment = body * (percent + (percent / (((1 + percent)**countMonths) - 1)));
-  // let payment = body * (mPercent + (mPercent / (((1 + mPercent)**countMonths) - 1)));
-  console.log(payment.toFixed(2));
+  let mPayment = body * (mPercent + (mPercent / (((1 + mPercent)**countMonths) - 1)));
+  let overall = Number((mPayment*countMonths).toFixed(2));
+  console.log(overall);
+  return overall;
 }
-// Напишите функцию, которая будет принимать процентную ставку, сумму первоначального взноса, сумму кредита и срок (длительность кредита в месяцах) в качестве аргументов и выдавать сумму, которую в итоге заплатит клиент (первоначальный взнос, погашение основного долга, проценты за пользование кредитом). 
-// 1. Реализуйте функцию `calculateTotalMortgage`, которая принимает четыре аргумента: `percent`, `contribution`, `amount` и `countMonths`.
-// 2. Преобразуйте процентную ставку из диапазона от 0 до 100 в диапазон от 0 до 1 и из годовой ставки — в месячную.
-// 3. Посчитайте тело кредита — сумму, которую нужно вернуть банку (сумма кредита минус первоначальный взнос).
-// 4. Ежемесячная оплата рассчитывается по формуле: `Платёж = S * (P + (P / (((1 + P)^n) - 1)))`, где:
-// `S` — тело кредита, `P` — 1/12 процентной ставки (от 0 до 1), `n` — количество месяцев, `^` — возведение в степень. Для возведения в степень используйте оператор — `**` или функцию [Math.pow](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/pow).
-// 5. Посчитайте общую сумму, которую придётся заплатить клиенту.
-// 6. Округлите результат до двух значений после запятой. Результат округления должен быть **числом**.
-// 7. Верните результат из функции. Результатом функции должно быть значение числового типа.
