7a4bc0a5704cc2f76ae089934a8b44f44fc9ca76
RealBAV <blazhevich.anton@gmail.com>
2023-06-27 21:06:41 +0300

    Решение задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discr = b ** 2 - 4 * a * c;
+	let rad = 0;
+	if (discr === 0) {
+		rad = -b / (2 * a);
+		arr.push(rad);
+	} else if (discr > 0) {
+		rad = (-b + Math.sqrt(discr) )/(2*a);
+		arr.push(rad);
+		rad = (-b - Math.sqrt(discr) )/(2*a);
+		arr.push(rad);
+	}
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthsPercent = percent / 100 / 12;
+	let body = amount - contribution;
+	let P = monthsPercent;
+	let monthsPaymant = body * (P + (P / (((1 + P) ** countMonths) - 1)));
+	let totalSum = (monthsPaymant * countMonths).toFixed(2);
+	return Number(totalSum);
 }
