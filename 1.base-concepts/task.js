30214af34665f145ba2dd7e31a486b359c17c89e
gukasyan <mishagukasyan@mail.ru>
2023-06-30 11:49:50 +0700

    change TASK

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const desk = (b**2)-4*a*c;
+
+  if (desk === 0) {
+    arr[0] = -b/(2*a);
+  };
+
+  if (desk > 0) {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a);
+    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+  };  
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+const percent1 = percent/100;
+const P = percent1/12;
+const S = amount - contribution;
+const monthPayment = S * (P + (P / (((1 + P)**countMonths) - 1)));
+const total = monthPayment * countMonths;
+
+return total.toFixed(2)
 }
