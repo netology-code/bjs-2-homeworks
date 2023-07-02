a1ab413bdf343ec4feb3cd0ba2ee605ccad38873
RealBAV <blazhevich.anton@gmail.com>
2023-07-02 20:11:37 +0300

    Решение задания 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,93 @@
-function getArrayParams(...arr) {
+//1 задача
 
+function getArrayParams(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		sum = sum + arr[i];
+	}
+	let avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
+//2 задача
 
+function summElementsWorker(...arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum = sum + arr[i]
+	}
+	return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let min = Infinity;
+	let max = -Infinity;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	let difference = max - min;
+	return difference;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			sumEvenElement = sumEvenElement + arr[i];
+		} else {
+			sumOddElement = sumOddElement + arr[i];
+		}
+	}
+	return (sumEvenElement - sumOddElement);
 }
 
 function averageEvenElementsWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			sumEvenElement = sumEvenElement + arr[i];
+			countEvenElement ++;
+		}
+	}
+	return (sumEvenElement / countEvenElement);
 }
 
-function makeWork (arrOfArr, func) {
+//3 задача
 
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
+	for (let i = 0; i < arrOfArr.length; i++) {
+		const funcResult = func(...arrOfArr[i]);
+		if (maxWorkerResult < funcResult) {
+			maxWorkerResult = funcResult;
+		}
+	}
+	return maxWorkerResult;
 }
