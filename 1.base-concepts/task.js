58dc4574d8c029102926e28fbbf8ca992b34cd22
4ucheba <122563295+4ucheba@users.noreply.github.com>
2023-07-18 22:09:32 +0300

    Update task.js
    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	if (a === 0) return ["Уравнение не квадратное"];
+
+	let answer = [];
+	let d = Math.pow(b, 2) - 4 * a * c
+	if (d < 0) return answer;
+	if (d === 0) {
+		answer = [-b / (2 * a)];
+	} else if (d > 0) {
+		answer.push((-b + Math.sqrt(d)) / (2 * a));
+		answer.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+	return answer;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentHundred = percent / 100;
+	let monthPercent = percentHundred / 12;
+	let creditBody = amount - contribution;
+
+	let pay = creditBody * (monthPercent + (monthPercent / (Math.pow(1 + monthPercent, countMonths) - 1)));
+
+	let summaryPay = +(pay * countMonths).toFixed(2);
+
+	return summaryPay;
 }
