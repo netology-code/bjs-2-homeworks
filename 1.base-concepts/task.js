6b338efe7ca983b7962f790ebd5b999d99c7bda9
Vallydei <obedkova.v@rambler.ru>
2023-08-29 23:00:15 +0300

    tests passed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2-4*a*c;
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    let answer = -b / (2*a);
+    arr.push(answer);
+    return arr;
+  } else {
+    let answer = (-b + Math.sqrt(d))/(2*a);
+    arr.push(answer);
+    answer = (-b - Math.sqrt(d))/(2*a);
+    arr.push(answer);
+    return arr;
+  }
   
-  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let sum = 0;
+  let monthPercentage = percent / 12 / 100;
+  let sumOfDebt = amount - contribution;
+  let monthlyPaying = sumOfDebt * (monthPercentage+(monthPercentage/(((1+monthPercentage)**countMonths)-1)));
+  sum = +(monthlyPaying * countMonths).toFixed(2);   
+  return sum;
+
 }
