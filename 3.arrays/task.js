c8aac2633cb5f4ace769660b19be27fb0f71d06a
Rina <61822400+coffetime-maker@users.noreply.github.com>
2023-08-31 00:23:06 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
-function compareArrays(arr1, arr2) {
+function compareArrays(arr1, arr2) {  
+    return (JSON.stringify(arr1) === JSON.stringify(arr2));
+  }
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function getUsersNamesInAgeRange(users, gender) {
+    return users.filter(user => user.gender === gender).map(result => result.age).reduce((summ, userAge, index, array) => {
+      summ += userAge;
+      if (index === array.length - 1) {
+        return summ / array.length;
+      }
+      return summ;
+    }, 0);
+  }
