25ea2a25e53809de4dbf12681d93065445f2f214
Rina <61822400+coffetime-maker@users.noreply.github.com>
2023-08-30 23:30:02 +0300

    updt_task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
+/* Задача №1 */
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  const discriminant = b**2 - 4 * a * c;
+  if (discriminant > 0){
+      return [(- b + Math.sqrt(discriminant)) / (2 * a), (- b - Math.sqrt(discriminant)) / (2 * a)];
+  } else if (discriminant === 0) {
+      return [- b / (2 * a)];
+  }
+  return [];
 }
 
+
+/* Задача №2 */
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent)) {
+    return false;
+  }
+
+  if (isNaN(contribution)) {
+    return false;
+  }
+
+  if (isNaN(amount)) {
+    return false;
+  }
+
+  let monthlyPercentage = ((percent/100)/12);
+  let loanBody = (amount-contribution);
+  let montlyPayment = (loanBody*(monthlyPercentage+(monthlyPercentage/(((1+monthlyPercentage)**countMonths)-1))));
+  let totalAmount = (montlyPayment*countMonths);
+
+  return +(totalAmount.toFixed(2));
 }
+    
+console.log(calculateTotalMortgage(10, 0, 50000, 12));
