d2893d7cd62b7eb9aedbff55a6caa62852853e0b
Zhan Teivans <118687635+JanTey@users.noreply.github.com>
2023-09-01 16:27:29 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((val, i) => val === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let sample = users.filter(user => user.gender === gender).map(old => old.age);
+    let result = sample.reduce((sum, age) => sum += age, 0) / sample.length;
+    return (sample.length > 0) ? result : 0;
 }
