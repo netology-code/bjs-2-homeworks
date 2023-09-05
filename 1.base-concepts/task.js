fd17f74347b84560480de34224904bd8c7c3b2f5
coremraz <reason1337@mail.ru>
2023-09-05 12:35:44 +0300

    fix

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,42 +1,34 @@
 /* Задача №1 */
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let D = (b ** 2) - (4 * a * c);
-       if (D < 0) {
-            arr.push();
-       } else if (D == 0) {
-          let quadraticRoots = -b/(2*a);
-              arr.push(quadraticRoots);
-       } else if (D > 0){
-    let tmp = [];
-          let firstRoot = (-b + Math.sqrt(D)) / (2 * a);
-          let secondRoot = (-b - Math.sqrt(D)) / (2 * a);
-               arr.push(firstRoot, secondRoot);
-  }
-   return arr;
+	let arr = [];
+	let D = (b ** 2) - (4 * a * c);
+	if (D < 0) {
+		arr.push();
+	} else if (D == 0) {
+		let quadraticRoots = -b / (2 * a);
+		arr.push(quadraticRoots);
+	} else if (D > 0) {
+		let firstRoot = (-b + Math.sqrt(D)) / (2 * a);
+		let secondRoot = (-b - Math.sqrt(D)) / (2 * a);
+		arr.push(firstRoot, secondRoot);
+	}
+	return arr;
 }
 
 /* Задача №2 */
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-   let totalAmount;
-   let percentTest = Number(percent);
-   let contributionTest = Number(contribution);
-   let amountTest = Number(amount);
-       if (percentTest != Number(percent)){
-           return false ;	
-   } else if (contributionTest != Number(contribution)){
-          return false; 
-   } else if (amountTest != Number(amount)){
-          return false; 
-   }
-     else {
-         let P = ((percent / 12) / 100);
-         let S = amount - contribution;
-         let payment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
+	let percentTest = Number(percent);
+	if (percentTest != Number(percent)) {
+		return false;
+	} else {
+		let P = ((percent / 12) / 100);
+		let S = amount - contribution;
+		let payment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
 
-         return Number(parseFloat(payment * countMonths).toFixed(2))
-      };
-     }
+		return Number(parseFloat(payment * countMonths).toFixed(2))
+	};
+}
 
-console.log(calculateTotalMortgage(10, 0, 50000, 12)) 
+console.log(calculateTotalMortgage(10, 0, 50000, 12))
