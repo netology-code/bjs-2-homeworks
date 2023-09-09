beed1b47d36048c0ebeae486ebcd8062a691aede
Radkovskaya Anna <lulali_7@mail.ru>
2023-09-10 02:02:41 +0300

    Первоеb и воторое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = (b ** 2) - (4 * a * c);
+	if (d === 0) {
+		arr.push(-b / (2 * a))
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyRate = (percent / 12) / 100;
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (((1 + monthlyRate) ** countMonths) - 1)));
+  let totalPaymentAmount = +(monthlyPayment * countMonths).toFixed(2);
+  return totalPaymentAmount;
 }
+
+
+
+
+
+
+
+
+
+
+
