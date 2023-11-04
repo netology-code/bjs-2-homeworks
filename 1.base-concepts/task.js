4bcf2db3fe0bd5b3d7e6d05d31cb5fb797f2ab44
MaxHugoboss <m.gorelov.91@yandex.ru>
2023-11-04 21:08:43 +0600

    Добавлен файл task.js с решенными задачами

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		return [];
+	} else if (d === 0) {
+		const root = -b / (2 * a);
+		return [root];
+	} else {
+		const root1 = (-b + Math.sqrt(d)) / (2 * a);
+		const root2 = (-b - Math.sqrt(d)) / (2 * a);
+		return [root1, root2];
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	percent = percent / 100 / 12; // преобразование процентной ставки
+	const loanBody = amount - contribution; // тело кредита
+	const monthlyPayment = loanBody * (percent + (percent / (((1 + percent) ** countMonths) - 1))) // ежемесячная оплата
+	const totalAmount = monthlyPayment * countMonths; // общая сумма
+	const roundingTotalAmount = totalAmount.toFixed(2); // округление до 2 знаков после запятой
+	return roundingTotalAmount;
 }
+
+const result = calculateTotalMortgage(10, 0, 50000, 12);
+console.log('Общая сумма выплат: ' + result);
