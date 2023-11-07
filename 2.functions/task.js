23d653adc71ce1afe242f9fb0f2ef0944751322b
Denis Paramonov <denis_paramon@icloud.com>
2023-11-07 11:01:42 +0500

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -77,19 +77,6 @@ function averageEvenElementsWorker(...arr) {
   return sumEvenElement / countEvenElement;
 }
 
-// Примеры использования
-console.log(summElementsWorker()); // 0
-console.log(summElementsWorker(10, 10, 11, 20, 10)); // 61
-
-console.log(differenceMaxMinWorker()); // 0
-console.log(differenceMaxMinWorker(10, 10, 11, 20, 10)); // 10
-
-console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17)); // 53
-console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // -269
-
-console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)); // 5
-console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // 38
-
 function makeWork(arrOfArr, func) {
   let maxWorkerResult = -Infinity; // Инициализируем переменную начальным значением -Infinity
 
@@ -111,8 +98,3 @@ const arr = [
   [72, 75, 51, 87, 43],
   [30, 41, 55, 96, 62]
 ];
-
-console.log(makeWork(arr, summElementsWorker)); // Максимум из 61, 206, 328, 284 => 328
-console.log(makeWork(arr, differenceMaxMinWorker)); // Максимум из 10, 86, 44, 66 => 86
-console.log(makeWork(arr, differenceEvenOddWorker)); // Максимум из 39, -6, -184, 92 => 92
-console.log(makeWork(arr, averageEvenElementsWorker)); // Максимум из 12.5, 33.333, 72, 62.66 => 72
