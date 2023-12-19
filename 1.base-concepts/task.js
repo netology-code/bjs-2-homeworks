9987343c55aea2096b19847af8b3d1fe95bcf10b
admin <promitay@gmail.com>
2023-12-19 17:34:33 +0300

    finish

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
+console.log(calculateTotalMortgage(10, 1000, 50000, 12))
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let arr = [];
+    let d = b ** 2 - 4 * a * c;
+    if (d === 0) {
+        arr.push(-b / (2 * a));
+    }
+    if (d > 0) {
+        arr.push((-b + Math.sqrt(d)) / (2 * a));
+        arr.push((-b - Math.sqrt(d)) / (2 * a));
+    }
+
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    let p = percent / 1200;
+    let s = amount - contribution;
+    let payment = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+    let sumPayment = payment * countMonths;
+    if (!Number.isInteger(sumPayment)) {
+        sumPayment = +sumPayment.toFixed(2);
+    }
+    return sumPayment;
 }
