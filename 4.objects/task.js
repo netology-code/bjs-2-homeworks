d73fd8f3ccfaec6b91be84fe351e3f699ad5bd61
Banachos <gosha.romanovich.2016@mail.ru>
2023-12-20 13:06:37 +0300

    last commit2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.marks = []
 }
-
-Student.prototype.setSubject = function (subjectName) {
-  
+let student = new Student("Igor","male",21)
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName
 }
-
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+    if(this.marks === undefined){
+        console.log("студент отчислен")
+    } else{
+    this.marks.push(...marksToAdd)
+    }
 }
-
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+    if (this.marks.length === 0) {
+        return 0
+    } 
+    const sum = this.marks.reduce((total, mark) => total + mark, 0);
+    return sum / this.marks.length
 }
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks
+    this.excluded = reason
 }
