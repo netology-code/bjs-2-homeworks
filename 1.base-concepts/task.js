de66103f40a728afa665ae56ce8284fa81a0fcf2
xzotex <61540705+xzotex@users.noreply.github.com>
2024-05-21 10:33:59 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,17 @@
-"use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+    'use strict'; // Активируем строгий режим
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    // Вычисляем дискриминант
+    const d = b * b - 4 * a * c;
+
+    if (d > 0) {
+        // Два различных корня
+        return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+    } else if (d == 0) {
+        // Один корень
+        return [-b / (2 * a)];
+    } else {
+        // Нет корней
+        return [];
+    }
 }
