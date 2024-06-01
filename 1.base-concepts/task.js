df29dd3d4bff7752a9b1cecebc68dd6f8633dc29
nikolaevna.spb <nikolaevna.spb@mail.ru>
2024-06-01 23:03:49 +0300

    Первое задание полность готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+
+	if (d > 0) {
+		let x1 = (-b + Math.sqrt(d)) / (2 * a);
+		let x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1, x2);
+		return arr;
+	} else if (d === 0) {
+		let x = -b / (2 * a);
+		arr.push(x);
+		return arr;
+	} else {
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let P = percent / 100 / 12;
+	let S = amount - contribution;
+	let monthlyPayment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
+	let totalAmount = monthlyPayment * countMonths;
+	return +totalAmount.toFixed(2);
 }
